8b062d19942f66e1d1476d8683c84e8665472375
Andra Uritu <andra_ioana.uritu@stud.fils.upb.ro>
2023-12-10 11:24:52 +0200

    cleaned up the folders

--- server/src/routes/readShoppingListRoute copy.js
+++
@@ -1,7 +0,0 @@
-const UserModel = require('../models/UserModel.js');
-
-module.exports = async (req, res) => {
-    const items = await ItemModel.find({});
-    res.json(items);
-
-}
--- server/src/routes/readShoppingListRoute.js
+++
@@ -1,7 +0,0 @@
-// const ItemModel = require('../models/ItemModel.js');
-
-// module.exports = async (req, res) => {
-//     const items = await ItemModel.find({});
-//     res.json(items);
-
-// }
